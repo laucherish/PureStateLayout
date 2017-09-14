caa5e6ecb3f7211b4f918b075bc010f19aa40a9a
laucherish <laucherish@gmail.com>
2017-09-14 11:50:23 +0800

    Add Demo

--- app/src/main/java/com/laucherish/purestatelayout/demo/MainActivity.java
+++ app/src/main/java/com/laucherish/purestatelayout/demo/MainActivity.java
@@ -1,13 +1,67 @@
 package com.laucherish.purestatelayout.demo;
 
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+import com.laucherish.purestatelayout.PureStateLayout;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private PureStateLayout mStateLayout;
+    private Button mBtnEmpty, mBtnError, mBtnLoading, mBtnContent;
+    private SwipeRefreshLayout mRefreshLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mRefreshLayout = (SwipeRefreshLayout) findViewById(R.id.swipe_refresh_layout);
+        mRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                mStateLayout.showLoading();
+                mStateLayout.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        if (mRefreshLayout.isRefreshing()) {
+                            mRefreshLayout.setRefreshing(false);
+                        }
+                        mStateLayout.showContent();
+                    }
+                }, 3000);
+            }
+        });
+        mStateLayout = (PureStateLayout) findViewById(R.id.pure_state_layout);
+        mBtnContent = (Button) findViewById(R.id.btn_content);
+        mBtnEmpty = (Button) findViewById(R.id.btn_empty);
+        mBtnError = (Button) findViewById(R.id.btn_error);
+        mBtnLoading = (Button) findViewById(R.id.btn_loading);
+        mBtnContent.setOnClickListener(this);
+        mBtnEmpty.setOnClickListener(this);
+        mBtnError.setOnClickListener(this);
+        mBtnLoading.setOnClickListener(this);
+
+        getData();
+    }
+
+    private void getData(){
+        mStateLayout.showLoading();
+        mStateLayout.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                if (mRefreshLayout.isRefreshing()) {
+                    mRefreshLayout.setRefreshing(false);
+                }
+                mStateLayout.showError();
+            }
+        }, 3000);
+    }
+
+    @Override
+    public void onClick(View view) {
+
     }
 }
