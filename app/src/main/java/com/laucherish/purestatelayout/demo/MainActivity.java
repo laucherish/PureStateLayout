68c91347e71e23bef4c6846d6e476e896ae63de6
laucherish <laucherish@gmail.com>
2017-09-16 16:59:10 +0800

    Publish v1.3.0

--- app/src/main/java/com/laucherish/purestatelayout/demo/MainActivity.java
+++ app/src/main/java/com/laucherish/purestatelayout/demo/MainActivity.java
@@ -86,6 +86,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 if (mRefreshLayout.isRefreshing()) {
                     mRefreshLayout.setRefreshing(false);
                 }
+
                 mStateLayout.showError();
             }
         }, 3000);
