90e65d8e716e06c79e52ea504bd9122f55b2f89d
laucherish <laucherish@gmail.com>
2017-10-23 19:12:25 +0800

    Publish v1.3.0

--- app/src/main/java/com/laucherish/purestatelayout/demo/MainActivity.java
+++ app/src/main/java/com/laucherish/purestatelayout/demo/MainActivity.java
@@ -94,7 +94,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     public void onClick(View view) {
         switch (view.getId()) {
-
             case R.id.btn_empty:
                 mStateLayout.showEmpty();
                 break;
